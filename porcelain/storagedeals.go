9b8c1b21200126aec7ecb28c1e8f156e6265b1fb
shannonwells <shannonwells@users.noreply.github.com>
2019-05-13 15:56:35 -0700

    add client show-deal, maybe needs to be part of main CLI ?

--- porcelain/storagedeals.go
+++ porcelain/storagedeals.go
@@ -10,7 +10,7 @@ type strgdlsPlumbing interface {
 	DealsLs() ([]*storagedeal.Deal, error)
 }
 
-// DealGet returns a single deal matching a given cid or an error
+// DealGet returns a single deal matching a given cid or nil
 func DealGet(plumbing strgdlsPlumbing, dealCid cid.Cid) *storagedeal.Deal {
 	deals, err := plumbing.DealsLs()
 	if err != nil {
